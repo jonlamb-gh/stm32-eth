4c8f0bc97ee4c83348420bf31499f6ed2950b431
Jon Lamb <lamb.jon.io@gmail.com>
2019-12-14 07:52:44 -0800

    smoltcp phy device caps

--- src/smoltcp_phy.rs
+++ src/smoltcp_phy.rs
@@ -1,10 +1,9 @@
+use crate::{rx::RxPacket, tx::TxError, Eth};
 use core::intrinsics::transmute;
 use core::ops::Deref;
-use smoltcp::Error;
+use smoltcp::phy::{Device, DeviceCapabilities, RxToken, TxToken};
 use smoltcp::time::Instant;
-use smoltcp::phy::{DeviceCapabilities, Device, RxToken, TxToken};
-use crate::{Eth, rx::RxPacket, tx::TxError};
-
+use smoltcp::Error;
 
 /// Use this Ethernet driver with [smoltcp](https://github.com/m-labs/smoltcp)
 impl<'a, 'rx, 'tx, 'b> Device<'a> for &'b mut Eth<'rx, 'tx> {
@@ -12,7 +11,10 @@ impl<'a, 'rx, 'tx, 'b> Device<'a> for &'b mut Eth<'rx, 'tx> {
     type TxToken = EthTxToken<'a>;
 
     fn capabilities(&self) -> DeviceCapabilities {
-        DeviceCapabilities::default()
+        let mut caps = DeviceCapabilities::default();
+        caps.max_transmission_unit = 1500;
+        caps.max_burst_size = Some(core::cmp::min(4, 4));
+        caps
     }
 
     fn receive(&mut self) -> Option<(Self::RxToken, Self::TxToken)> {
@@ -27,26 +29,25 @@ impl<'a, 'rx, 'tx, 'b> Device<'a> for &'b mut Eth<'rx, 'tx> {
                 let tx = EthTxToken { eth };
                 Some((rx, tx))
             }
-            Err(_) =>
-                None
+            Err(_) => None,
         }
     }
 
     fn transmit(&mut self) -> Option<Self::TxToken> {
-        let eth = unsafe {
-            transmute::<&mut Eth<'rx, 'tx>, &mut Eth<'a, 'a>>(*self) as *mut Eth<'a, 'a>
-        };
+        let eth =
+            unsafe { transmute::<&mut Eth<'rx, 'tx>, &mut Eth<'a, 'a>>(*self) as *mut Eth<'a, 'a> };
         Some(EthTxToken { eth })
     }
 }
 
 pub struct EthRxToken<'a> {
-    packet: RxPacket<'a>
+    packet: RxPacket<'a>,
 }
 
 impl<'a> RxToken for EthRxToken<'a> {
     fn consume<R, F>(self, _timestamp: Instant, f: F) -> Result<R, Error>
-        where F: FnOnce(&[u8]) -> Result<R, Error>
+    where
+        F: FnOnce(&[u8]) -> Result<R, Error>,
     {
         let result = f(self.packet.deref());
         self.packet.free();
@@ -57,19 +58,19 @@ impl<'a> RxToken for EthRxToken<'a> {
 /// Just a reference to [`Eth`](../struct.Eth.html) for sending a
 /// packet later with [`consume()`](#method.consume)
 pub struct EthTxToken<'a> {
-    eth: *mut Eth<'a, 'a>
+    eth: *mut Eth<'a, 'a>,
 }
 
 impl<'a> TxToken for EthTxToken<'a> {
     /// Allocate a [`Buffer`](../struct.Buffer.html), yield with
     /// `f(buffer)`, and send it as an Ethernet packet.
     fn consume<R, F>(self, _timestamp: Instant, len: usize, f: F) -> Result<R, Error>
-        where F: FnOnce(&mut [u8]) -> Result<R, Error>
+    where
+        F: FnOnce(&mut [u8]) -> Result<R, Error>,
     {
         let eth = unsafe { &mut *self.eth };
         match eth.send(len, f) {
-            Err(TxError::WouldBlock) =>
-                Err(Error::Exhausted),
+            Err(TxError::WouldBlock) => Err(Error::Exhausted),
             Ok(r) => r,
         }
     }
